a4594bfc3fe36cf6860d00962717a3006e474ee6
peierlong <peielcn@gmail.com>
2020-06-08 18:58:11 +0800

    feat: continue

---
+++ src/views/Article.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div v-html="compiledMarkdown" />
+  </div>
+</template>
+<script>
+    import marked from 'marked'
+
+    let rendererMD = new marked.Renderer();
+    marked.setOptions({
+        renderer: rendererMD,
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: false,
+        smartLists: true,
+        smartypants: false
+    })
+    export default {
+        name: 'App',
+        data () {
+            return {
+                input: '# hello `java` '
+            }
+        },
+        computed: {
+            compiledMarkdown: function () {
+                return marked(this.input, { sanitize: true })
+            }
+        }
+    }
+</script>
