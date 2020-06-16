72c012bb36a8f519f361136c418c453be4343cc2
peierlong <peielcn@gmail.com>
2020-06-16 13:50:01 +0800

    feat: 修改标签文章的打开方式

--- src/components/item.vue
+++ src/components/item.vue
@@ -37,9 +37,9 @@
                 console.log('test')
                 console.log(this.aId)
                 if (this.$route.path.indexOf('manager') === -1) {
-                    window.open('/article/' + this.aId, '_blank') // 新窗口打开外链接
+                    window.location.href = '/article/' + this.aId;
                 } else {
-                    window.location.href = '/manager/article/' + this.aId;
+                    window.open('/manager/article/' + this.aId, '_blank') // 新窗口打开外链接
                 }
             }
         }
